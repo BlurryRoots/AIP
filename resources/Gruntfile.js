fd6e2cd72451820c9c32f815ae4d4c1f53307024
Max Zender <mail@maxzender.de>
2014-11-07 15:54:23 +0100

    let nodemon reload the server when changes occur
    - Grunt is now only used for actually compiling the
      coffee-script files via 'grunt build'

--- resources/Gruntfile.js
+++ resources/Gruntfile.js
@@ -3,60 +3,21 @@
 var request = require('request');
 
 module.exports = function (grunt) {
-	// show elapsed time at the end
-	require('time-grunt')(grunt);
-	// load all grunt tasks
-	require('load-grunt-tasks')(grunt);
-
-	var reloadPort = 35729, files;
-
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		develop: {
-			server: {
-				file: 'index.coffee'
+		coffee: {
+			glob_to_multiple: {
+				expand: true,
+				flatten: false,
+				cwd: 'app/',
+				src: ['**/*.coffee'],
+				dest: 'dist/',
+				ext: '.js'
 			}
-		},
-		watch: {
-			options: {
-				nospawn: true,
-				livereload: true
-			},
-			js: {
-				files: [
-					'app.coffee',
-					'app/**/*.coffee',
-					'config/*.coffee'
-				]
-			},
-			tasks: ['develop', 'delayed-livereload']
 		}
 	});
 
-	grunt.config.requires('watch.js.files');
-	files = grunt.config('watch.js.files');
-	files = grunt.file.expand(files);
-
-	grunt.registerTask('develop', 'What and why?', function () {
-		grunt.log.ok('This is kinda bullshit?!');
-	});
-
-	grunt.registerTask('delayed-livereload', 'Live reload after the node server has restarted.', function () {
-		var done = this.async();
-		setTimeout(function () {
-			request.get('http://localhost:' + reloadPort + '/changed?files=' + files.join(','),  function(err, res) {
-				var reloaded = ! err && res.statusCode === 200;
-				if (reloaded) {
-					grunt.log.ok('Delayed live reload successful.');
-				}
-				else {
-					grunt.log.error('Unable to make a delayed live reload.');
-				}
-				done(reloaded);
-			});
-		}, 500);
-	});
-
-	grunt.registerTask('default', ['develop']);
+	grunt.loadNpmTasks('grunt-contrib-coffee');
+	grunt.registerTask('build', ['coffee']);
 };
 
